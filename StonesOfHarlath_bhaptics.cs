2fcf6c0cc3be3042dd43cc026f0aa6f0b2a9ffe2
floh-bhaptics <floh@fahrenbergers.de>
2022-01-02 21:32:29 +0100

    Projektdateien hinzufügen.

---
+++ StonesOfHarlath_bhaptics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+
+using MyBhapticsTactsuit;
+
+namespace StonesOfHarlath_bhaptics
+{
+    public class StonesOfHarlath_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        /*
+        [HarmonyPatch(typeof(VertigoPlayer), "Die")]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
+    }
+}
